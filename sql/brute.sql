0cf12667a55d10a98b24ae6b77232b7273d5d605
NyckolasP <nicolas@allforweb.fr>
2022-09-18 23:54:39 +0200

    bdd

--- sql/brute.sql
+++ sql/brute.sql
@@ -16,6 +16,35 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `image` text NOT NULL,
+  `type` enum('arme','bouclier','tenue') NOT NULL,
+  `force` int(11) NOT NULL,
+  `esquive` int(11) NOT NULL,
+  `defense` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,'hache en pierre','','arme',1,0,0),(2,'Bouclier en bois','','bouclier',0,0,1),(3,'épée en pierre','','arme',2,0,0);
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `robot`
 --
 
@@ -49,6 +78,55 @@ INSERT INTO `robot` VALUES (1,'Ratchet',2,1,0,10,1,0,50,'nico@gmail.com',11),(4,
 UNLOCK TABLES;
 
 --
+-- Table structure for table `robot_item`
+--
+
+DROP TABLE IF EXISTS `robot_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `robot_item` (
+  `id_robot` int(11) NOT NULL,
+  `id_item` int(11) NOT NULL,
+  PRIMARY KEY (`id_robot`,`id_item`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `robot_item`
+--
+
+LOCK TABLES `robot_item` WRITE;
+/*!40000 ALTER TABLE `robot_item` DISABLE KEYS */;
+INSERT INTO `robot_item` VALUES (13,1),(13,2);
+/*!40000 ALTER TABLE `robot_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `shop`
+--
+
+DROP TABLE IF EXISTS `shop`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `shop` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_item` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `shop`
+--
+
+LOCK TABLES `shop` WRITE;
+/*!40000 ALTER TABLE `shop` DISABLE KEYS */;
+INSERT INTO `shop` VALUES (1,1,10),(2,2,10),(3,3,15);
+/*!40000 ALTER TABLE `shop` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `utilisateur`
 --
 
@@ -62,7 +140,7 @@ CREATE TABLE `utilisateur` (
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +149,7 @@ CREATE TABLE `utilisateur` (
 
 LOCK TABLES `utilisateur` WRITE;
 /*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
-INSERT INTO `utilisateur` VALUES (14,'pendragon@gmail.com','test','test','test'),(13,'test@gmail.com','test','test','test'),(12,'test@gmail.com','test','test','test'),(11,'nico@gmail.com','test','Puechbroussoux','Nicolas');
+INSERT INTO `utilisateur` VALUES (15,'Nyckolaxx','test','test','test'),(14,'pendragon@gmail.com','test','test','test'),(13,'test@gmail.com','test','test','test'),(12,'test@gmail.com','test','test','test'),(11,'nico@gmail.com','test','Puechbroussoux','Nicolas');
 /*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -84,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-18 21:00:06
+-- Dump completed on 2022-09-18 23:53:34
